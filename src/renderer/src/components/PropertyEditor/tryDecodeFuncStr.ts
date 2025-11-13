a51953f058bdf3ef820fa12595d00b88b551c055
MornMartin <beingwrong@live.com>
2025-11-14 01:42:34 +0800

    处理属性控件参数项定义为对象方法时转回失败问题

--- src/renderer/src/components/PropertyEditor/tryDecodeFuncStr.ts
+++ src/renderer/src/components/PropertyEditor/tryDecodeFuncStr.ts
@@ -1,9 +1,14 @@
 export default (str: string) => {
     try {
         //unsafe-eval
-        const handler = eval(str);
+        const handler = eval(str);// 处理箭头函数
         return typeof handler === 'function' ? handler : str;
     } catch (err) {
-        return str;
+        try {
+            const handler = eval(`(function ${str})`);// 处理对象定义方法函数
+            return typeof handler === 'function' ? handler : str
+        } catch (err) {
+            return str;
+        }
     }
 }
