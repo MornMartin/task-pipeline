544778bbf4b0c5795ecbfa1ee8f07ff2c7ca3e3d
MornMartin <beingwrong@live.com>
2025-11-14 01:44:01 +0800

    属性控件添加数字输入框

---
+++ src/renderer/src/components/PropertyEditor/Ctrls/InputNumber/index.tsx
@@ -0,0 +1,53 @@
+import { InputNumber } from 'antd';
+import style from './index.module.less'
+import Label from '../Label/index';
+import { useContext, useEffect, useState } from 'react'
+import { IPropertyInputNumber } from '../../declare';
+import { PropertyGetterContext } from '../..';
+
+interface IProps {
+    define: IPropertyInputNumber;
+    value: number;
+    onChange: (e: number) => void;
+}
+
+const Component: React.FC<IProps & Record<string, any>> = (props): React.JSX.Element => {
+    const { define, value, onChange } = props;
+    const propertyGetter = useContext(PropertyGetterContext);
+
+    const [disabled, setDisabled] = useState<boolean>(false);
+    const [placeholder, setPlaceholder] = useState<string>('');
+    const [min, setMin] = useState<number>();
+    const [max, setMax] = useState<number>();
+    const [precision, setPrecision] = useState<number>();
+    const [controls, setControls] = useState<boolean>();
+
+    useEffect(() => {
+        setDisabled(propertyGetter(define.params?.disabled, define) || false);
+        setPlaceholder(propertyGetter(define.params?.placeholder, define) || '');
+        setMin(propertyGetter(define.params?.min, define) ?? undefined);
+        setMax(propertyGetter(define.params?.max, define) ?? undefined);
+        setPrecision(propertyGetter(define.params?.precision, define) ?? undefined);
+        setControls(propertyGetter(define.params?.controls, define) ?? undefined);
+    }, [propertyGetter, define]);
+
+    return (
+        <div className={style.InputNumber}>
+            {define?.label ? <Label label={define?.label}></Label> : null}
+            <InputNumber
+                value={value}
+                disabled={disabled}
+                placeholder={placeholder}
+                min={min}
+                max={max}
+                precision={precision}
+                controls={controls}
+                style={{ width: 'var(--ctrl-width)' }}
+                onChange={(e) => onChange(e as number)}
+            >
+            </InputNumber>
+        </div>
+    )
+}
+
+export default Component;
---
+++ src/renderer/src/components/PropertyEditor/Ctrls/InputNumber/validator.ts
@@ -0,0 +1,5 @@
+import { IPropertyInputNumber } from "../../declare";
+
+export default (ctrl: IPropertyInputNumber, values: Record<string, any>): Promise<boolean> | boolean => {
+    return true;
+}
